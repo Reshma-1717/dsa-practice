bc928d0d6f7b2ae70114cd46c7b9fdb3b5aa8dd0
Reshma-1717 <reshmakodavali@gmail.com>
2026-09-15 22:24:37 +0530

    Time: 100 ms (15.06%), Space: 22.3 MB (88.26%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return ""
+        s = []
+        q = deque()
+        q.append(root)
+        while q:
+            cur = q.popleft()
+            if cur is None:
+                s.append("#")
+            else:
+                s.append(str(cur.val))
+                q.append(cur.left)
+                q.append(cur.right)
+        return ",".join(s)     
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if not data:
+            return None
+        s = data.split(",")
+        root = TreeNode(int(s[0]))
+        q = deque([root])
+        # q.append([root])
+        i = 1
+        while q:
+            cur = q.popleft()
+            if s[i] != "#":
+                cur.left = TreeNode(int(s[i]))
+                q.append(cur.left)
+            i += 1
+            if s[i] != '#':
+                cur.right = TreeNode(int(s[i]))
+                q.append(cur.right)
+            i += 1
+        return root
+
+        
