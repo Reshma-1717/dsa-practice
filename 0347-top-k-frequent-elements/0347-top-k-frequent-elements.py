47ffe3ddafbfda93fdb3d294c0bfc09c143d9c5f
Reshma-1717 <reshmakodavali@gmail.com>
2026-09-18 19:20:11 +0530

    Time: 0 ms (100%), Space: 22.8 MB (80.98%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        c = Counter(nums)
+        c = dict(sorted(c.items(),key = lambda item:item[1],reverse = True))
+        arr = []
+        for key in c.keys():
+            if k > 0:
+                arr.append(key)
+                k -= 1
+        return arr
